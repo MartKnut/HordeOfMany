8049923a62f799c69b0eac9f8b3d2f8f0e893fb6
Martink <martin.knutsen@live.no>
2024-08-08 16:37:27 +0200

    Enemy Animations

--- Scripts/Enemy_Script.gd
+++ Scripts/Enemy_Script.gd
@@ -6,11 +6,25 @@ var player : Node
 var globalPosition
 
 var canAttack = true
+var animationSequence : int
+var deathAnimation = "Death1"
+
+var bigCollision : CollisionShape2D
+var smallCollision : CollisionShape2D
+var attackCollision : CollisionShape2D
+
+var animatedSprite : AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = $"../../PlayerScene"
+	bigCollision = $BigCollision
+	smallCollision = $SmallCollision
+	animatedSprite = $AnimatedSprite2D
+	attackCollision = $AttackCollision
 	
+	bigCollision.set_deferred("disabled", true)
+	attackCollision.set_deferred("disabled", true)
 
 func _enter_tree():
 	globalPosition = global_position.x
@@ -37,10 +51,32 @@ func _process(delta):
 				position.x = globalPosition-256
 			elif player.global_position.x >= 0:
 				position.x = globalPosition
+	
+	if animatedSprite.animation == "approach":
+		animationSequence = animatedSprite.frame
+	
+	match animationSequence:
+		0:
+			deathAnimation = "Death0"
+		6:
+			deathAnimation = "Death1"
+		12:
+			deathAnimation = "Death2"
+		14:
+			deathAnimation = "Death3"
+			smallCollision.set_deferred("disabled", true)
+			bigCollision.set_deferred("disabled", false)
+		16:
+			deathAnimation = "Death4"
+		19:
+			deathAnimation = "Death5"
+			
 
 func die():
 	canAttack = false
-	$CollisionShape2D.disabled = true
+	animatedSprite.play(deathAnimation)
+	bigCollision.disabled = true
+	smallCollision.disabled = true
 	$AudioStreamPlayer2D.play()
 	$DeathTimer.start()
 	$"../../GameManager".increaseScore()
@@ -55,4 +91,14 @@ func _on_death_timer_timeout():
 
 func _on_attack_timer_timeout():
 	if canAttack:
+		animatedSprite.play("attack")
+		attackCollision.set_deferred("disabled", false)
+		smallCollision.set_deferred("disabled", true)
+		bigCollision.set_deferred("disabled", true)
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if animatedSprite.animation == "attack" and canAttack:
 		$"../../PlayerScene".damage()
+	if animatedSprite.animation != "attack" and animatedSprite.animation != "approach":
+		animatedSprite.visible = false
